a2ed6f73f551bc32a755ead5dbe20dad7adb5af0
pandu.artyan <pandu.artyan@btpn.com>
2020-10-09 00:57:28 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+import 'react-native-gesture-handler';
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
+
+const App = () => (
+  <SafeAreaView>
+    <Text>Hello There</Text>
+  </SafeAreaView>
+)
+
+export default App;
